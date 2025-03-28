585ece0078596631a1c7cc5e6df07b8057c04e64
Xavier Bouclet <mikrethor@gmail.com>
2025-03-27 22:38:05 -0400

    Add keep object and classes consistent

---
+++ src/examples/object-and-classes-after.ts
@@ -0,0 +1,10 @@
+// Good example
+class User {
+    name: string;
+    email: string;
+}
+
+class EmailService {
+    sendEmail(user: User, message: string) {
+    }
+}
---
+++ src/examples/object-and-classes-before.ts
@@ -0,0 +1,8 @@
+// Bad example
+class User {
+    name: string;
+    email: string;
+
+    sendEmail(message: string) {
+    }
+}
