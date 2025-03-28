29247e1c8d7c008d7a4be7f60c2ad512550dc1f0
Xavier Bouclet <mikrethor@gmail.com>
2025-03-27 22:27:07 -0400

    Update error handling

--- src/examples/error-handling-before.ts
+++ src/examples/error-handling-before.ts
@@ -1,24 +1,9 @@
-import logger from './logger';
-
-class BusinessError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = 'BusinessError';
-    }
-}
-
 function someRiskyOperation(): void {
-    throw new BusinessError('Operation failed');
+    throw new Error("Operation failed");
 }
 
 try {
     someRiskyOperation();
-} catch (e: unknown) {
-    if (e instanceof BusinessError) {
-        logger.error(`[CustomError] ${e.message}`);
-    } else if (e instanceof Error) {
-        logger.error(`[Erreur] ${e.message}`);
-    } else {
-        logger.error('An unknown error occured');
-    }
+} catch (e) {
+    // ❌ Mauvaise pratique : l'erreur est ignorée
 }
