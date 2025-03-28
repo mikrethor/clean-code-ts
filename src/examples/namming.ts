b8fc17a59260a4889ce0252e0180eacbcbf9aac9
Xavier Bouclet <mikrethor@gmail.com>
2025-03-27 22:27:13 -0400

    Add naming

---
+++ src/examples/namming.ts
@@ -0,0 +1,13 @@
+function calculateTotalPrice(): number {
+    return 0;
+}
+
+function getTP(): number {
+    return 0;
+}
+
+
+// Bad example
+let tp = getTP();
+// Good example
+let totalPrice = calculateTotalPrice();
